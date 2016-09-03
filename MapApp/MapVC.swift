245e65de325a5c6fee72abd8503b1741f1fef107
Donovan Cotter <donovan.cotter@yahoo.com>
2016-09-02 21:51:04 -0400

    initial storyboard and mapview functions with search capabilities setup

---
+++ MapApp/MapVC.swift
@@ -0,0 +1,125 @@
+//
+//  MapVC.swift
+//  MapApp
+//
+//  Created by Donovan Cotter on 9/2/16.
+//  Copyright © 2016 DonovanCotter. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+protocol HandleMapSearch: class {
+    func dropPinAtSearchedLocation(placemark:MKPlacemark)
+}
+
+class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, HandleMapSearch {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapStyleBarButton: UIBarButtonItem!
+    
+    private var resultSearchController:UISearchController? = nil
+    private var searchedLocation:MKPlacemark? = nil
+    private var locationManager: CLLocationManager?
+    private var newestLocation = CLLocation()
+    private var userLocation = MKCoordinateRegion()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupMapView()
+        setUpSearchControllerWithSearchTable()
+        setUpSearchBar()
+        getUserLocation()
+
+    }
+    
+    //MARK: Map Methods
+    
+    func setupMapView() {
+        guard let mapView = mapView else {return}
+        mapView.delegate = self
+        mapView.showsPointsOfInterest = false
+        mapView.showsUserLocation = true
+    }
+    
+    func adjustMapViewCamera() {
+        let newCamera = mapView.camera
+        
+        guard mapView.camera.pitch < 30.0 else {
+            newCamera.pitch = mapView.camera.pitch
+            return
+        }
+        newCamera.pitch = 30
+        self.mapView.camera = newCamera
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Creates SearchController
+    func setUpSearchControllerWithSearchTable()  {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let locationSearchTable = storyboard.instantiateViewControllerWithIdentifier("LocationSearchTVC") as! LocationSearchTVC
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+    }
+    
+    //Configures the Search Bar
+    func setUpSearchBar() {
+        let searchBar = resultSearchController?.searchBar
+        searchBar?.sizeToFit()
+        searchBar?.placeholder = "Search For Places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+    }
+    
+    //This drops the pin at the searched location when using the search bar.
+    func dropPinAtSearchedLocation(placemark:MKPlacemark) {
+        searchedLocation = placemark
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    //MARK: Location Methods
+    func getUserLocation() {
+        locationManager = CLLocationManager()
+        
+        guard let manager = locationManager else {
+            return
+        }
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.requestWhenInUseAuthorization()
+        manager.distanceFilter = 100
+        manager.startUpdatingLocation()
+        
+        guard let managerLocation = manager.location else {
+            return
+        }
+        newestLocation = managerLocation
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let lastLocation = locations.last else {
+            return
+        }
+        newestLocation = lastLocation
+        userLocation = MKCoordinateRegionMakeWithDistance(newestLocation.coordinate, 800, 800)
+        mapView.setRegion(userLocation, animated: true)
+    }
+
+
+}
---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MapApp' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+pod ‘Firebase’
+pod 'Firebase/Database'
+pod 'Firebase/Auth'
+pod 'Cloudinary'
+pod 'AlamofireImage', '~> 2.0'
+pod ’RealmSwift'
+
+  # Pods for MapApp
+
+  target 'MapAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MapAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
